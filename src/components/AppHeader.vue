77e5e6ecd556be090b2d15736c30f7ea72b380d2
Arnaud Groby <arnaud@MacBook-Pro-de-Arnaud.local>
2023-11-21 10:32:50 +0100

    initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-center">CLICK DROIT -> INSPECTER</h1>
+    <h2>Un couteau suisse bien rempli</h2>
+  </div>
+</template>
+
+<style scoped></style>
