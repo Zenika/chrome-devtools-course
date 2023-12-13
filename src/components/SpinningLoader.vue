00ae05943c467d38f6c8fc8c5d1d0d6979ff64bb
Arnaud Groby <arnaud.groby@zenika.com>
2023-12-13 17:46:42 +0100

    adding a loader for network throttling

---
+++ src/components/SpinningLoader.vue
@@ -0,0 +1,46 @@
+<template>
+  <span class="spinning-loader"></span>
+</template>
+
+<style scoped lang="scss">
+.spinning-loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  position: relative;
+  animation: rotate 1s linear infinite;
+}
+.spinning-loader::before {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  inset: 0;
+  border-radius: 50%;
+  border: 5px solid var(--color-secondary);
+  animation: prixClipFix 2s linear infinite;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes prixClipFix {
+  0% {
+    clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+  }
+  25% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+  }
+  50% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+  }
+  75% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+  }
+  100% {
+    clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+  }
+}
+</style>
--- src/views/NetworkView.vue
+++ src/views/NetworkView.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import PixelButton from '@/components/PixelButton.vue'
-import {Beer, getBeers} from '@/libs/api'
-import Slides from "@/components/Slides.vue";
-import Slide from "@/components/Slide.vue";
-import {ref} from "vue";
+import { type Beer, getBeers } from '@/libs/api'
+import Slides from '@/components/Slides.vue'
+import Slide from '@/components/Slide.vue'
+import { ref } from 'vue'
+import SpinningLoader from '@/components/SpinningLoader.vue'
 
 const beers = ref<Beer[]>([])
+const loading = ref(false)
 
 const onClickApiButton = async (): Promise<void> => {
   beers.value = await getBeers()
 }
 
+const onClickApiCall = async (): Promise<void> => {
+  try {
+    loading.value = true
+    await getBeers()
+  } catch (err) {
+    console.log(err)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
@@ -18,15 +30,16 @@ const onClickApiButton = async (): Promise<void> => {
     <Slide class="flex-column align-center">
       <p>Network Throttling, le retour au réseau Edge.</p>
       <div class="button-wrapper">
-        <PixelButton text="API call" :action="getBeers"/>
+        <PixelButton text="API call" :action="onClickApiCall" />
+        <SpinningLoader v-if="loading" />
       </div>
     </Slide>
     <Slide>
       <p>Network overrides, ou comment mocker ses API sans backend.</p>
       <div class="button-wrapper">
-        <PixelButton text="API call" :action="onClickApiButton"/>
+        <PixelButton text="API call" :action="onClickApiButton" />
         <span v-if="beers.length > 0" class="text"
-        >There is {{ beers.length }} beer<span v-if="beers.length > 1">s</span>.</span
+          >There is {{ beers.length }} beer<span v-if="beers.length > 1">s</span>.</span
         >
       </div>
     </Slide>
@@ -40,10 +53,16 @@ const onClickApiButton = async (): Promise<void> => {
   flex-direction: column;
   align-items: center;
   margin: 15px 0;
+  gap: 10px;
+  position: relative;
 
-  & .pixel-btn {
+  .pixel-btn {
     margin-bottom: 15px;
   }
-}
 
+  .spinning-loader {
+    position: absolute;
+    top: 60px;
+  }
+}
 </style>
